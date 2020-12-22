98215d4eead32d044d68477e18c1683bc96a9c1a
aimakerhk <59435476+stem-hanshin@users.noreply.github.com>
2020-12-22 11:04:52 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,25 @@
 
+//% color="#2c3e50" weight=10
+namespace ultrasonicsensor {
+    /**
+     * Send a ping and get the echo time (in microseconds) as a result
+     * @param trig tigger pin
+     * @param echo echo pin
+     * @param unit desired conversion unit
+     * @param maxCmDistance maximum distance in centimeters (default is 100)
+     */
+    //% blockId=sonar_ping block="ping trig %trig|echo %echo"
+    export function ping(trig: DigitalPin, echo: DigitalPin,  maxCmDistance = 100): number {
+        // send pulse
+        pins.setPull(trig, PinPullMode.PullNone);
+        pins.digitalWritePin(trig, 0);
+        control.waitMicros(2);
+        pins.digitalWritePin(trig, 1);
+        control.waitMicros(10);
+        pins.digitalWritePin(trig, 0);
+
+        // read pulse
+        const d = pins.pulseIn(echo, PulseValue.High, maxCmDistance * 58);
+        return Math.idiv(d, 58);
+    }
+}
